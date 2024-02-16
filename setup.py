798fea024366c1c33e1102707010631f004d3755
Federico Rapetti <f.rapetti@reply.it>
2024-02-16 18:29:16 +0100

    Add long description

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     name=PACKAGE_NAME,
     version="0.1.0",
     description="Use Azure AI Language to generate abstractive summaries of documents.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     entry_points={
         "package_tools": ["custom_text_classification = custom_text_classification_tool.tools.utils:list_package_tools"],
